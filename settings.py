62976a8952168e38ad03d5e7f9e16f80700d898d
Rohan Bansal <rohan.bansal6@gmail.com>
2012-03-30 11:58:34 -0400

    fixed caps

--- settings.py
+++ settings.py
@@ -99,7 +99,7 @@ MIDDLEWARE_CLASSES = (
 	'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'tigerchat.urls'
+ROOT_URLCONF = 'TigerChat.urls'
 
 TEMPLATE_DIRS = (
 	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'tigerchat.views.home', name='home'),
+    url(r'^$', 'TigerChat.views.home', name='home'),
     # url(r'^tigerchat/', include('tigerchat.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
