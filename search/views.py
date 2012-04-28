3a575662b44d76f669697999b845f03936abd940
Santhosh Balasubramanian <santhoshbala@gmail.com>
2012-04-28 15:57:05 -0400

    created search app and moved over files from communication

---
+++ search/views.py
@@ -0,0 +1,135 @@
+# Create your views here.
+
+
+## search(): Returns list of all people that match query term
+## at the moment, cannot search by individual fields
+def search_ldap(request):
+    # Create command from search query term
+    query = request.GET.get('query')
+   # cmd = '/usr/bin/ldapsearch -x -h ldap.princeton.edu -u -b o=\'Princeton University,c=US\' \"(cn=*%s*)\" uid givenName sn purescollege puclassyear puhomedepartmentnumber' % query
+    #fin, fout = os.popen4(cmd)
+    # Get all entries from ldapsearch
+    
+    #entries = fout.read().split('#')[8:-3]
+    fout = getoutput('/usr/bin/ldapsearch -x -h ldap.princeton.edu -u -b o="Princeton University, c=US" "(cn=*%s*)" uid givenName sn purescollege puclassyear puhomedepartmentnumber' % query)
+    entries = fout.split('#')[8:-3]
+#   data = {}
+    data = []
+#   data['results'] = []
+    # Parse each entry to create dict
+    for entry in entries:
+        result = {}
+        entry = entry.strip().split('\n')
+        # Parse each line in entry
+        for line in entry:
+            fields = line.split()
+            if fields[0] == 'uid:':
+                try:
+                    result['username'] = fields[1]
+                except:
+                    continue
+            elif fields[0] == 'givenName:':
+                try:
+                    result['first_name'] = fields[1]
+                except:
+                    continue
+            elif fields[0] == 'sn:':
+                try:
+                    result['last_name'] = fields[1]
+                except:
+                    continue
+            elif fields[0] == 'purescollege:':
+                try:
+                    result['dorm'] = fields[1]
+                except:
+                    continue
+            elif fields[0] == 'puclassyear:':
+                try:
+                    result['class'] = fields[1]
+                except:
+                    continue
+            elif fields[0] == 'puhomedepartmentnumber:':
+                try:
+                    result['dept'] = fields[1]
+                except:
+                    continue
+            else:
+                continue
+#       data['results'].append(result)
+        data.append(result)
+
+
+    for POI in data:
+        person_name = POI.get("username")
+        # check if person is an existing user
+        poi_user = User.objects.filter(username=person_name)
+        if len(poi_user) == 0: # no matching user
+            POI['friendship_status'] = 'DNE'
+        # result matches existing user
+        elif len(poi_user) == 1: 
+            poi_user = poi_user[0]
+            # check if person is a friend
+            friendship = Friendship.objects.filter( Q(creator=request.user.person, receiver=poi_user.person) |
+                                                    Q(creator=poi_user.person, receiver=request.user.person))
+            if len(friendship) == 0:
+                POI['friendship_status'] = 'None'
+            elif len(friendship) == 1:
+                # check if I have sent a request to person
+                if friendship[0].status == 'Confirmed':
+                    POI['friendship_status'] = 'Confirmed'
+                else:       # check if I have sent a request
+                    if friendship[0].creator == request.user.person:
+                        POI['friendship_status'] = 'Pending'
+                    else:
+                        POI['friendship_status'] = 'To_Accept'
+            else:
+                raise Exception('more than one user with username')
+
+    response = simplejson.dumps(data, default=json_handler)
+    return HttpResponse(response, mimetype='application/javascript')
+
+
+# returns a dictionary representing user_netid's ldap record
+def get_ldap_record(user_netid):
+    fout = getoutput('/usr/bin/ldapsearch -x -h ldap.princeton.edu -u -b o="Princeton University, c=US" "(mail=%s@princeton.edu)"' % user_netid)
+    entries = fout.split('#')[8:-3]
+    entry = entries[0]
+    result = {}
+    # Parse each entry to create dict
+    entry = entry.strip().split('\n')
+    # Parse each line in entry
+    for line in entry:
+        fields = line.split()
+        if fields[0] == 'uid:':
+            try:
+                result['username'] = fields[1]
+            except:
+                continue
+        elif fields[0] == 'givenName:':
+            try:
+                result['first_name'] = fields[1]
+            except:
+                continue
+        elif fields[0] == 'sn:':
+            try:
+                result['last_name'] = fields[1]
+            except:
+                continue
+        elif fields[0] == 'purescollege:':
+            try:
+                result['dorm'] = fields[1]
+            except:
+                continue
+        elif fields[0] == 'puclassyear:':
+            try:
+                result['year'] = fields[1]
+            except:
+                continue
+        elif fields[0] == 'puhomedepartmentnumber:':
+            try:
+                result['dept'] = fields[1]
+            except:
+                continue
+        else:
+            continue
+    return result
--- urls.py
+++ urls.py
@@ -11,21 +11,32 @@ urlpatterns = patterns('',
     url(r'^$', 'TigerChat.views.home', name='home'),
     url(r'^time/$', 'TigerChat.views.datetime'),
     url(r'^tigerchat/$', 'TigerChat.views.tigerchat_main'),
+    
     url(r'^login/$', 'django_cas.views.login'),
     url(r'^logout/$', 'django_cas.views.logout'),
-    url(r'^search/$', 'TigerChat.communication.views.search'),
+    
+    url(r'^search/$', 'TigerChat.communication.views.search_ldap'),
+    url(r'^search/students/$', 'TigerChat.search.views.search_students'),
+    url(r'^search/rooms/$', 'TigerChat.communication.views.search_rooms'),
+    
+    
     url(r'^friends/$', 'TigerChat.communication.views.get_friends'),
     url(r'^pending/$', 'TigerChat.communication.views.get_pending'),
     url(r'^requests/$', 'TigerChat.communication.views.get_requests'),
     url(r'^invite/$', 'TigerChat.communication.views.invite_user'),
     url(r'^addfriend/$', 'TigerChat.communication.views.add_friend'),
+    url(r'^group/create/$', 'TigerChat.communication.views.create_group'),
+    url(r'^group/add/$', 'TigerChat.communication.views.add_person_to_group'),
+    url(r'^group/remove/$','TigerChat.communication.views.remove_group'),
+    url(r'^room/create$', 'TigerChat.communication.views.create_room'),
+    url(r'^room/invite$', 'TigerChat.communication.views.invite_person_to_room'),
+    url(r'^room/leave$', 'TigerChat.communication.views.leave_room'),
+    
     url(r'^register/$', 'TigerChat.registration.views.register_new_user'),
     url(r'^users/$', 'TigerChat.registration.views.users'),
     url(r'^people/$', 'TigerChat.registration.views.people'),
     url(r'^removeall/$', 'TigerChat.registration.views.remove_everything'),
-                       #url(r'^newtime/$', 'TigerChat.views.new_datetime'),
-                       # url(r'^tigerchat/', include('tigerchat.foo.urls')),
-
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
