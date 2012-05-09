f789c44a113efacc8bb4dc71d30af52d824b7ca1
Santhosh Balasubramanian <santhoshbala@gmail.com>
2012-05-09 17:01:26 +0000

    old ways are best.

--- static/admin/js/admin/ordering.js
+++ static/admin/js/admin/ordering.js
@@ -11,7 +11,7 @@ function reorder_init() {
     setOrder(input.value.split(','));
     input.disabled = true;
     draw();
-    // Now initialize the dragging behavior
+    // Now initialise the dragging behaviour
     var limit = (lis.length - 1) * height;
     for (var i = 0; i < lis.length; i++) {
         var li = lis[i];
